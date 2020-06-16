36c32f36fb169ac10fa08a9cd72f1f6a04770170
David Springate <david@liquidbridge.net>
2020-06-16 20:09:47 +0100

    First working version
    XZCompress will break up a file into a user specified chunk size, compress the chunks separately and write out JSON describing each chunk.
    It'll also try each of DEFLATE's compression strategies and use the one that produces the smallest size.
    The output JSON file appends data for different files so that it becomes a collated collection of compressed chunk descriptions for say, a whole collection of data rather than one file.
    If a file is already in the JSON data, it's record is overwritten.
    
    Tested across a few file types in debug and release.

--- XZCompress/XZCompress.cpp
+++ XZCompress/XZCompress.cpp
@@ -8,6 +8,12 @@
 #include <string.h>
 #include <assert.h>
 
+#include <fstream> // For Json parsers :/
+
+// External libraries:
+#include <zlib.h>
+#include <json/json.h>
+
 void printHeader()
 {
 	printf("XZCompress\n");
@@ -24,7 +30,7 @@ long int getFileSize(FILE* fh)
 	int returnVal = fseek(fh, 0, SEEK_END);
 	if (returnVal != 0)
 	{
-		printf("An error occurred during seek.");
+		printf("An error occurred during seek.\n");
 		return 0;
 	}
 
@@ -36,7 +42,7 @@ void seekToBeginning(FILE* fh)
 	int returnVal = fseek(fh, 0, SEEK_SET);
 	if (returnVal != 0)
 	{
-		printf("An error occurred during seek.");
+		printf("An error occurred during seek.\n");
 	}
 }
 
@@ -45,33 +51,88 @@ void seekForwardBy(FILE* fh, long int offset)
 	int returnVal = fseek(fh, offset, SEEK_CUR);
 	if (returnVal != 0)
 	{
-		printf("An error occurred during seek.");
+		printf("An error occurred during seek.\n");
 	}
 
 }
 
-FILE* openFileForReading(const char* inputFilePath)
+FILE* openFileForReadingBinary(const char* inputFilePath)
 {
 	FILE* inputFileHandle = nullptr;
 	{
 		errno_t fopenRetVal = fopen_s(&inputFileHandle, inputFilePath, "r+b");
 		if (fopenRetVal != 0)
 		{
-			printf("Error opening %s for reading.", inputFilePath);
+			printf("Error opening %s for reading.\n", inputFilePath);
 		}
 	}
 	return inputFileHandle;
 }
 
-FILE* openFileForWriting(const char* outputFilePath)
+FILE* openFileForReadingText(const char* inputFilePath)
+{
+	FILE* inputFileHandle = nullptr;
+	{
+		errno_t fopenRetVal = fopen_s(&inputFileHandle, inputFilePath, "r");
+		if (fopenRetVal != 0)
+		{
+			printf("Error opening %s for reading.\n", inputFilePath);
+		}
+	}
+	return inputFileHandle;
+}
+
+FILE* openFileForWritingBinary(const char* outputFilePath)
 {
 	FILE* outputFileHandle = nullptr;
 	errno_t fopenRetVal = fopen_s(&outputFileHandle, outputFilePath, "w+b");
 	if (fopenRetVal != 0)
 	{
-		printf("Error opening %s for writing.", outputFilePath);
+		printf("Error opening %s for writing.\n", outputFilePath);
 	}
 	return outputFileHandle;
+}
+
+
+FILE* openFileForWritingText(const char* outputFilePath)
+{
+	FILE* outputFileHandle = nullptr;
+	errno_t fopenRetVal = fopen_s(&outputFileHandle, outputFilePath, "w");
+	if (fopenRetVal != 0)
+	{
+		printf("Error opening %s for writing.\n", outputFilePath);
+	}
+	return outputFileHandle;
+}
+
+bool deflate_with_strategy(z_stream& myZStream, const unsigned int strategy, unsigned int currentChunkSize, unsigned char* chunkData, unsigned char* compressedDataBuffer)
+{
+	myZStream.zalloc = Z_NULL;
+	myZStream.zfree = Z_NULL;
+	myZStream.opaque = Z_NULL;
+	myZStream.avail_in = currentChunkSize;
+	myZStream.next_in = chunkData;
+	myZStream.avail_out = currentChunkSize;
+	myZStream.next_out = compressedDataBuffer;
+	myZStream.data_type = Z_BINARY;
+
+	int windowBits = 15;
+	int deflateInitReturnVal = deflateInit2(&myZStream, Z_BEST_COMPRESSION, Z_DEFLATED, windowBits, 9, strategy);
+
+	int deflateReturnVal = deflate(&myZStream, Z_FINISH);
+	if ((deflateReturnVal != Z_STREAM_END) && (deflateReturnVal != Z_OK))
+	{
+		printf("An error occurred during deflate.\n");
+		return false;
+	}
+
+	if (myZStream.total_out >= myZStream.total_in)
+	{
+		printf("Compressed data size is >= input data size.");
+		return false;
+	}
+
+	return true;
 }
 
 int main(int argc, char** argv)
@@ -107,17 +168,34 @@ int main(int argc, char** argv)
 	printf("Writing metadata to %s\n", outputMetaDataFilePath);
 
 	// Open input file:
-	FILE* inputFileHandle = openFileForReading(inputFilePath);
+	FILE* inputFileHandle = openFileForReadingBinary(inputFilePath);
 	if (!inputFileHandle)
 	{
 		return 1;
 	}
 
 	// Open output file:
-	FILE* outputFileHandle = openFileForWriting(outputFilePath);
+	FILE* outputFileHandle = openFileForWritingBinary(outputFilePath);
 	if (!outputFileHandle)
 	{
 		return 1;
+	}
+
+	Json::Value rootJsonValue;
+
+	// Does the metadata file already exist?
+	std::ifstream ifs(outputMetaDataFilePath);
+	if (ifs.is_open())
+	{
+		// If it already exists, populate our JSON data:
+		Json::CharReaderBuilder builder;
+		std::string errs;
+		bool parsingReturnValue = Json::parseFromStream(builder, ifs, &rootJsonValue, &errs);
+		if (!parsingReturnValue)
+		{
+			printf("An existing Json file was found but an error occurred during parsing.\n");
+			return 1;
+		}
 	}
 
 	// How big is our input file?
@@ -133,6 +211,12 @@ int main(int argc, char** argv)
 	seekToBeginning(inputFileHandle);
 	seekToBeginning(outputFileHandle);
 
+	Json::Value newJsonValue;
+	//newJsonValue["filename"] = inputFilePath;
+	newJsonValue["requested_chunk_size"] = requestedChunkSize;
+	newJsonValue["number_of_chunks"] = numberOfChunks;
+
+	// Before we start, let's initialise Zlib:
 	for (unsigned int i = 0; i < numberOfChunks; ++i)
 	{
 		const unsigned int currentChunkSize = (i < numberOfWholeChunks) ? chunkSize : lastChunkSize;
@@ -144,8 +228,6 @@ int main(int argc, char** argv)
 			return 1;
 		}
 		memset(chunkData, 0, currentChunkSize);
-
-		//printf("Current file position: %d\n", ftell(inputFileHandle));
 
 		size_t readSize = fread(chunkData, currentChunkSize, 1, inputFileHandle);
 		if (ferror(inputFileHandle))
@@ -160,22 +242,114 @@ int main(int argc, char** argv)
 		}
 
 		// Compress the chunk:
+		unsigned char* compressedDataBuffer = (unsigned char*)malloc(currentChunkSize);
+
+		// We're going to run compression 5 times, in order to get the best
+		// compression for smallest size.
+		size_t smallestCompressedDataSize = currentChunkSize;
+		char bestCompressionMethod = 0;
+
+		for (unsigned int i = 0; i < 5; ++i)
+		{
+			// Success isn't guaranteed and that's okay..
+			z_stream myZStream = {};
+			deflate_with_strategy(myZStream, i, currentChunkSize, chunkData, compressedDataBuffer);
+
+			if (myZStream.total_out < smallestCompressedDataSize)
+			{
+				smallestCompressedDataSize = myZStream.total_out;
+				bestCompressionMethod = i;
+			}
+		}
+
+		// Now run it again so we get the actual data:
+		{
+			z_stream myZStream = {};
+			const bool deflateResult = deflate_with_strategy(myZStream, bestCompressionMethod, currentChunkSize, chunkData, compressedDataBuffer);
+			if (deflateResult == false)
+			{
+				return 1;
+			}
+
+			if (myZStream.total_out >= myZStream.total_in)
+			{
+				printf("Compressed data size is >= input data size.");
+				return 1;
+			}
+		}
+
+		z_stream myZStream = {};
+		myZStream.zalloc = Z_NULL;
+		myZStream.zfree = Z_NULL;
+		myZStream.opaque = Z_NULL;
+		myZStream.avail_in = currentChunkSize;
+		myZStream.next_in = chunkData;
+		myZStream.avail_out = currentChunkSize;
+		myZStream.next_out = compressedDataBuffer;
+		myZStream.data_type = Z_BINARY;
+
+		int windowBits = 15;
+		int deflateInitReturnVal = deflateInit2(&myZStream, Z_BEST_COMPRESSION, Z_DEFLATED, windowBits, 9, Z_DEFAULT_STRATEGY);
+
+		int deflateReturnVal = deflate(&myZStream, Z_FINISH);
+		if ((deflateReturnVal != Z_STREAM_END) && (deflateReturnVal != Z_OK))
+		{
+			printf("An error occurred during deflate.\n");
+			return 1;
+		}
+
+		if (myZStream.total_out >= myZStream.total_in)
+		{
+			printf("Compressed data size is >= input data size.");
+			return 1;
+		}
 
 		// Write out some meta data to describe this chunk to JSON:
+		printf("Compressed %d chunk to %d bytes.\n", currentChunkSize, myZStream.total_out);
+		newJsonValue["chunks"][i]["chunk_size_uncompressed"] = currentChunkSize;
+		newJsonValue["chunks"][i]["chunk_size_compressed"] = (unsigned int)myZStream.total_out;
+		//newJsonValue["chunks"][i]["chunk_size_saved_difference"] = (unsigned int)(currentChunkSize - myZStream.total_out);
+		//newJsonValue["chunks"][i]["percent_saved"] = 100 - (((float)myZStream.total_out / (float)currentChunkSize) * 100);
 
-		// Write out to disk:
-		size_t elementsWritten = fwrite(chunkData, currentChunkSize, 1, outputFileHandle);
+		// Write out compressed data:
+		size_t elementsWritten = fwrite(compressedDataBuffer, myZStream.total_out, 1, outputFileHandle);
 		if (elementsWritten != 1)
 		{
 			printf("A write error occurred.\n");
 			return 1;
 		}
 
+		// Close Zlib handle:
+		int deflateEndReturnVal = deflateEnd(&myZStream);
+		if (deflateEndReturnVal != Z_OK)
+		{
+			printf("An error occurred calling deflateEnd().\n");
+			return 1;
+		}
+
+		free(compressedDataBuffer);
 		free(chunkData);
 	}
+
+	// Check for duplicate entries, remove them first:
+	if (rootJsonValue.isMember(inputFilePath))
+	{
+		rootJsonValue.removeMember(inputFilePath);
+	}
+	rootJsonValue[inputFilePath].append(newJsonValue);
+
+	// Write out Json data:
+	Json::StreamWriterBuilder builder;
+	builder["commentStyle"] = "None";
+	builder["indentation"] = "   ";
+
+	std::unique_ptr<Json::StreamWriter> writer(builder.newStreamWriter());
+	std::ofstream outputFileStream(outputMetaDataFilePath, std::ofstream::out);
+	writer->write(rootJsonValue, &outputFileStream);
 
 	fclose(outputFileHandle);
 	fclose(inputFileHandle);
 
 	return 0;
 }
+	
